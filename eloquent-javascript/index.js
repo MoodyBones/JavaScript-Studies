d44186917a49bfa1f8092a9fc6ae26b2fea6b314
Mel <jones.mel@gmail.com>
2020-01-21 18:43:04 +0100

    FizzBuzzFunction - working on this

--- eloquent-javascript/index.js
+++ eloquent-javascript/index.js
@@ -3,15 +3,58 @@
 
 // Looping a Triangle
 
-let loopTo7Function = () => {
-  let hash = '#'
+const loopTo7Function = () => {
+  let hashString = '#'
   let hashArray = []
   let current = ''
 
   for (let i = 0; i < 7; i++) {
-    current = current + hash
+    current = current + hashString
     hashArray.push(current)
   }
   return hashArray.forEach(element => console.log(element))
 }
 loopTo7Function()
+
+// FizzBuzz
+
+// print numbers from 1 to 100
+// any number divisible by 3 print Fizz
+// any number divisible by 5 print Buzz
+// any number divisible by both 3 & 5 print FizzBuzz
+
+// lets write the math functions
+
+function divisionFunction(number) {
+  let check3 = number / 3
+  let check5 = number / 5
+  if (
+    check3 === parseInt(check3, 10) &&
+    check3 >= 1 &&
+    check5 === parseInt(check5, 10) &&
+    check5 >= 1
+  ) {
+    return 'FizzBuzz'
+  } else if (check3 === parseInt(check3, 10) && check3 >= 1) {
+    return 'Fizz'
+  } else if (check5 === parseInt(check5, 10) && check5 >= 1) {
+    return 'Buzz'
+  }
+  return number
+}
+
+function doDivision(array) {
+  array.forEach(number => divisionFunction(number))
+  return
+}
+
+function FizzBuzzFunction() {
+  let hundredArray = [1]
+
+  for (let i = 0; i < 99; i++) {
+    hundredArray.push(Number(hundredArray.slice(-1)) + hundredArray[0])
+  }
+  doDivision(hundredArray)
+
+  console.log(hundredArray)
+}
