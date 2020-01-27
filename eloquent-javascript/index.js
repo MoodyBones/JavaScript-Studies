5e55a0c063259a96189ba3750db202f5968fe981
Mel <jones.mel@gmail.com>
2020-01-27 15:34:16 +0100

    work in progress

--- eloquent-javascript/index.js
+++ eloquent-javascript/index.js
@@ -51,7 +51,7 @@ function getNumbers(array) {
 function FizzBuzzFunction() {
   let hundredArray = [1]
 
-  for (let i = 0; i < 99; i++) {
+  for (let i = 1; i < 100; i++) {
     hundredArray.push(Number(hundredArray.slice(-1)) + hundredArray[0])
   }
   getNumbers(hundredArray)
@@ -65,3 +65,42 @@ function FizzBuzzFunction() {
 
 // change loop function, so that it checks the numbers as it goes
 // if it's fizz or buzz or fizzbuzz check slice(-2) + 2
+
+//  next try
+// it stops at 1 now
+
+function FizzBuzzFunction() {
+  let hundredArray = []
+  let check3
+  let check5
+
+  for (let i = 3; hundredArray.length <= 100; i++) {
+    check3 = i / 3
+    check5 = i / 5
+
+    // console.log(check3)
+    // console.log(check5)
+
+    if (
+      check3 === parseInt(check3, 10) &&
+      check3 >= 1 &&
+      check5 === parseInt(check5, 10) &&
+      check5 >= 1
+    ) {
+      console.log('FizzBuzz')
+      return hundredArray.push('FizzBuzz')
+    } else if (check3 === parseInt(check3, 10)) {
+      console.log('Fizz')
+      return hundredArray.push('Fizz')
+    } else if (check5 === parseInt(check5, 10)) {
+      console.log('Buzz')
+
+      return hundredArray.push('Buzz')
+    } else {
+      return hundredArray.push(i)
+    }
+  }
+  console.log(hundredArray)
+}
+
+FizzBuzzFunction()
