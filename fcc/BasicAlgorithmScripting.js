1f983eec9c389b4426717b9c46cdb9d37c3a6458
Mel <jones.mel@gmail.com>
2020-04-20 00:32:04 +0200

    refactor plus 2 more exercises

--- fcc/BasicAlgorithmScripting.js
+++ fcc/BasicAlgorithmScripting.js
@@ -2,15 +2,16 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/
 
 //////////////////////////////////
+// 1
 // convert celsius to fahrenheit
 function convertToF(celsius) {
-  let fahrenheit = (celsius * 9) / 5 + 32
-  return fahrenheit
+  return (celsius * 9) / 5 + 32
 }
 
-convertToF(30)
+console.log(convertToF(30))
 
 ///////////////////////////////////
+// 2
 // Reverse a string
 
 // my answer
@@ -21,6 +22,7 @@ function reverseString(str) {
 reverseString('hello')
 
 //////////////////////////////
+// 3
 // Factorilise (5 = 1 * 2 * 3 * 4 * 5)
 
 function factorialize(num) {
@@ -45,7 +47,8 @@ function factorialize(num) {
 factorialize(5)
 
 //////////////////////////////////////
-// Find the Longest Word in a String
+// 4
+// Find the length of the longest Word in a String
 
 // my answer
 function findLongestWordLength(str) {
@@ -58,6 +61,17 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength('The quick brown fox jumped over the lazy dog')
+
+// refactor using reduce
+function findLongestWordLengthV2(str) {
+  return str.split(' ').reduce((longestWord, currentWord) => {
+    return currentWord.length > longestWord.length ? currentWord : longestWord
+  }, '').length
+}
+
+console.log(
+  findLongestWordLengthV2('The quick brown fox jumped over the lazy dog')
+)
 
 // or with map
 function findLongestWordLength(str) {
@@ -79,6 +93,7 @@ function findLongestWordLength(str) {
 }
 
 ///////////////////////////////
+// 5
 // Return Largest Numbers in Arrays
 
 // answer
@@ -109,3 +124,51 @@ function largestOfFour(arr) {
 function largestOfFour(arr) {
   return arr.map(Function.apply.bind(Math.max, null))
 }
+
+//////////////////////////////////////////
+// 6
+// Confirm the Ending
+// time taken: 36mins
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target
+}
+
+console.log(confirmEnding('Bastian', 'n')) // true
+console.log(confirmEnding('Connor', 'n')) // false
+console.log(confirmEnding('He has to give me a new name', 'name')) // true
+console.log(confirmEnding('Open sesame', 'pen')) // false
+console.log(confirmEnding('Abstraction', 'action')) // true
+
+//////////////////////////////////////////
+// 7
+// Repeat a String
+// don't use replace()
+// time taken: 10 mins
+// refactor time: 13 mins
+
+// original
+// function repeatStringNumTimes(str, num) {
+//   let result = []
+//   for (let i = 0; i < num; i++) {
+//     result.push(str)
+//   }
+//   return num <= 0 ? '' : result.join('')
+// }
+
+// refactor
+function repeatStringNumTimes(str, num) {
+  let result = ''
+  for (let i = 0; i < num; i++) {
+    result += str
+  }
+  return num > 0 ? result : ''
+}
+
+console.log(repeatStringNumTimes('abc', 3))
+
+//////////////////////////////////////////
+// 8
+//
+// time taken:
+// refactor time:
