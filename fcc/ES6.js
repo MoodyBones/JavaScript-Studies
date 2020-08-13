234d3aea72cc13d826faa6441022e805bce9379c
Mel <jones.mel@gmail.com>
2020-08-13 15:02:17 +0200

    template literals, object decs & dec fn

--- fcc/ES6.js
+++ fcc/ES6.js
@@ -6,7 +6,8 @@
 // Differences between var and let
 // ////
 // One of the biggest problems with declaring variables with the var keyword is that you can overwrite variable declarations without an error.
-//  A new keyword called let was introduced in ES6 to solve this potential issue with the var keyword. If you were to replace var with let in the variable declarations of the code above, the result would be an error.
+//  A new keyword called let was introduced in ES6 to solve this potential issue with the var keyword. 
+// If you were to replace var with let in the variable declarations of the code above, the result would be an error.
 
 let catName
 let quote
@@ -259,6 +260,176 @@ function removeFirstTwo(list) {
   return arr;
 }
 const arr = removeFirstTwo(source);
+
+
+
+// ////
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+// sometimes you don't want to pass in the whole object - only the need fields
+// you can use destructuring in a function argument
+// and added benefit of not having to manipulate an entire object in a fucntion
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0; 
+// Only change code above this line
+
+
+
+// ////
+// Create strings using Template Literals
+
+// a template literal is a special type of strign that makes creatign complex strings easier
+// they allow you to create multi-line strings and to use string interpolation features to create strings
+
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+
+// Template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting); // prints
+// Hello, my name is Zodiac Hasbro!
+// I am 56 years old.
+
+// A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. 
+// Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. 
+
+// The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. 
+// To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. 
+
+// Similarly, you can include other expressions in your string literal, for example ${a + b}. 
+// This new way of creating strings gives you more flexibility to create robust strings.
+
+
+
+
+// Exercise
+// Use template literal syntax with backticks to display each entry of the result object's failure array. 
+// Each entry should be wrapped inside an li element with the class attribute text-warning, and listed within the resultDisplayArray.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+// function makeList(arr) {
+//   "use strict";
+
+//   const resultDisplayArray = arr.map(x => `<li class="text-warning">${x}</li>`)
+//   return resultDisplayArray;
+// }
+
+// refactored
+function makeList(arr) {
+  "use strict";
+
+  return arr.map(x => `<li class="text-warning">${x}</li>`)
+}
+
+const resultDisplayArray = makeList(result.failure);
+
+
+
+// ////
+// Write Concise Object Literal Declarations - using object property shorthand
+
+//before
+// const getMousePosition = (x, y) => ({
+//   x: x,
+//   y: y
+// });
+
+
+// with object literals
+const getMousePosition = (x, y) => ({ x, y });
+
+
+// Write Concise Declarative Functions with ES6
+
+// When defining functions within objects in ES5, we have to use the keyword function as follows:
+// const person = {
+//   name: "Taylor",
+//   sayHello: function() {
+//     return `Hello! My name is ${this.name}.`;
+//   }
+// };
+
+//  in ES6 you can remove the function keyword and colon
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+
+// exercise
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+
+
+// CLass syntax
+
+// ES6 provides a new syntax to create objects, using the class keyword.
+
+// It should be noted that the class syntax is just syntax, and not a full-fledged class-based implementation of an object-oriented paradigm, unlike in languages such as Java, Python, Ruby, etc.
+
+// In ES5, we usually define a constructor function and use the new keyword to instantiate an object.
+
+var SpaceShuttle = function(targetPlanet){
+  this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+
+// The class syntax simply replaces the constructor function creation:
+
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+}
+const zeus = new SpaceShuttle('Jupiter');
+
+// It should be noted that the class keyword declares a new function, to which a constructor is added. This constructor is invoked when new is called to create a new object.
+
+// UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above.
+// The constructor method is a special method for creating and initializing an object created with a class. You will learn more about it in the Object Oriented Programming section of the JavaScript Algorithms And Data Structures Certification.
+
+
+// exercise
+class Vegetable {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+
+
+
+
 
 
 
