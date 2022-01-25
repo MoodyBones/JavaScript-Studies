dc8996c6b5d7587640915930a9524aea68a3b349
Mel Jones <mel.jones@hyretalents.com>
2022-01-25 12:40:54 +0100

    notes to process

---
+++ hard-parts-will-sentance/pairprogramming.js
@@ -0,0 +1,9 @@
+// I know what a variable is
+// - I've created a function before
+// - I've added a CSS style before
+// - I have implemented a sort algorithm (bubble, merge etc)
+// - I can add a method to an object’s prototype
+// - I understand the event loop in JavaScript
+// - I understand 'callback functions'
+// - I can implement filter
+// - I can handle collisions in a hash table
